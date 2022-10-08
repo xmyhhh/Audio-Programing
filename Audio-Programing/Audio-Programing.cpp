6affef52cfac16487e862b4e22f2be4c7c4eff16
xmyhhh <xmycissy@outlook.com>
2022-10-08 17:51:02 +0800

    1

--- Audio-Programing/Audio-Programing.cpp
+++ Audio-Programing/Audio-Programing.cpp
@@ -1,20 +1,70 @@
-// Audio-Programing.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
+#include <cmath>
+#include <fstream>
+using namespace std;
 
-#include <iostream>
+const int sampleRate = 44100;
+const int bitDepth = 16;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class SineOscillator {
+    float frequency, amplitude, angle = 0.0f, offset = 0.0f;
+public:
+    SineOscillator(float freq, float amp) : frequency(freq), amplitude(amp) {
+        offset = 2 * 3.1415 * frequency / sampleRate;
+    }
+    float process() {
+        auto sample = amplitude * sin(angle);
+        angle += offset;
+        return sample;
+        // Asin(2pif/sr)
+    }
+};
+
+void writeToFile(ofstream& file, int value, int size) {
+    file.write(reinterpret_cast<const char*> (&value), size);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main() {
+    int duration = 2;
+    ofstream audioFile;
+    audioFile.open("waveform.wav");
+    SineOscillator sineOscillator(440, 0.5);
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    //Header chunk
+    audioFile << "RIFF";
+    audioFile << "----";
+    audioFile << "WAVE";
+
+    // Format chunk
+    audioFile << "fmt ";
+    writeToFile(audioFile, 16, 4); // Size
+    writeToFile(audioFile, 1, 2); // Compression code
+    writeToFile(audioFile, 1, 2); // Number of channels
+    writeToFile(audioFile, sampleRate, 4); // Sample rate
+    writeToFile(audioFile, sampleRate * bitDepth / 8, 4); // Byte rate
+    writeToFile(audioFile, bitDepth / 8, 2); // Block align
+    writeToFile(audioFile, bitDepth, 2); // Bit depth
+
+    //Data chunk
+    audioFile << "data";
+    audioFile << "----";
+
+    int preAudioPosition = audioFile.tellp();
+
+    auto maxAmplitude = pow(2, bitDepth - 1) - 1;
+    for (int i = 0; i < sampleRate * duration; i++) {
+        auto sample = sineOscillator.process();
+        int intSample = static_cast<int> (sample * maxAmplitude);  // 32 -> 16 float
+        writeToFile(audioFile, intSample, 2);
+    }
+    int postAudioPosition = audioFile.tellp();
+
+    audioFile.seekp(preAudioPosition - 4);
+    writeToFile(audioFile, postAudioPosition - preAudioPosition, 4);
+
+    audioFile.seekp(4, ios::beg);
+    writeToFile(audioFile, postAudioPosition - 8, 4);  //-8是因为 id和size不包括在内
+
+    audioFile.close();
+    return 0;
+}
